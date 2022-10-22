960127b5c2435999627330ee20daf5c1f6ae1569
lxhroot <lxh94088@petalmail.com>
2022-10-22 23:50:44 +0800

    学期项目V0.0.3

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// 乐购商城首页js
+// 当页面加载完毕
+$(function () {
+    // 首页大图轮播
+    $('#bannerInner').tyslide({
+        boxh:460,//盒子的高度
+        w:1000,//盒子的宽度
+        h:390,//图片的高度
+        isShow:true,//是否显示控制器
+        isShowBtn:true,//是否显示左右按钮
+        controltop:40,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW:20,//控制按钮宽度
+        controlsH:20,//控制按钮高度
+        radius:10,//控制按钮圆角度数
+        controlsColor:"#d7d7d7",//普通控制按钮的颜色
+        controlsCurrentColor:"#ff6600",//当前控制按钮的颜色
+        isShowNum:true //是否显示数字
+    });
+})
